7a68e1f744b54195afb9039748e12380090403c5
leedonggyu1848 <leedong1848@gmail.com>
2025-11-23 22:11:56 +0900

    rebuild: 2025-11-23T22:11:56+0900

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 echo -e "\033[0;32mDeploying updates to GitHub\033[0m"
  
 # 모든 서브모듈의 변경사항을 업데이트
-echo -e "\033[0;32mdUpdating all submodules\033[0m"
+echo -e "\033[0;32mUpdating all submodules\033[0m"
 git submodule update --remote --init
  
 # 프로젝트 빌드
 # `hugo -t <테마명>`
-echo -e "\033[0;32mdBuilding the hugo project\033[0m"
+echo -e "\033[0;32mBuilding the hugo project\033[0m"
 hugo -t papermod
  
 # `source` 브랜치로 이동
-echo -e "\033[0;32mdUpdating the branch with build files\033[0m"
+echo -e "\033[0;32mUpdating the branch with build files\033[0m"
 cd public
 git add .
 # 인자가 없을 경우 현재 시간을 커밋 메시지로 등록
@@ -23,7 +23,7 @@ git commit -m "$msg"
 git push origin source
  
 # 현재까지의 변경사항을 `main` 브랜치에 반영
-echo -e "\033[0;32mdUpdating the main branch\033[0m"
+echo -e "\033[0;32mUpdating the main branch\033[0m"
 cd ..
 git add .
 if [ $# -eq 1 ]
